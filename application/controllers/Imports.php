f6a5d6dba0fb2f119779578cac155d460b2fc886
ferroxido <ferroxido@gmail.com>
2016-03-25 19:22:55 -0300

    created imports views

--- application/controllers/Imports.php
+++ application/controllers/Imports.php
@@ -2,21 +2,52 @@
 
 class Imports extends CI_Controller {
 
+    protected $imports_path = './imports-csv/';
+
     //Constructor
     function __construct(){
         parent::__construct();
         $this->load->model('Model_alumnos');
+        $this->load->helper('directory');
+        $this->load->helper('file');
     }
 
     public function index(){
         $data['contenido'] = 'imports/index';
+
+        $files_name = directory_map($this->imports_path);
+        $files_imported = array();
+        foreach ($files_name as $file_name) {
+            $path = $this->imports_path . $file_name;
+            $files_imported[] = get_file_info($path);
+        }
+
+        $data['files_imported_data'] = $files_imported;
         $this->load->view('tmp-admin', $data);
     }
 
-    public function insert($go){
-        $registro = $this->input->post();
-        $this->Model_dias->update($registro);
-        redirect('almuerzos/index/'.$go);
+    public function import(){
+        if($this->session->userdata('dni_usuario') != null){
+            $config['upload_path'] = './imports-csv/';
+            $config['allowed_types'] = 'csv';
+            $config['max_size'] = '2048';
+            $config['max_width']  = '0';
+            $config['max_height']  = '0';
+            $config['overwrite'] = true;
+            //$config['file_name'] = 'test';
+
+            $this->load->library('upload', $config);
+
+            if (! $this->upload->do_upload()){
+                //Controlar error al subir archivo.
+                $mostrar = true;
+                $error = "No se pudo subir la imagen ".$this->upload->display_errors();
+
+                echo $error;
+            }else{
+                redirect('imports/index');
+            }
+        }
     }
 
 }
--- application/views/imports/index.php
+++ application/views/imports/index.php
@@ -1,1 +1,32 @@
-hola mundo
+<?= form_open_multipart('imports/import', array()); ?>
+  <div class="form-group">
+    <label for="exampleInputFile">File input</label>
+    <input type="file" name="userfile" id="exampleInputFile">
+    <p class="help-block">Solo se puede importar archivos de tipo csv.</p>
+  </div>
+  <button type="submit" class="btn btn-primary">Importar</button>
+<?= form_close(); ?>
+
+<h2>Files</h2>
+
+<table class="table table-bordered table-striped table-hover">
+    <thead>
+        <tr>
+            <th> Nombre Archivo </th>
+            <th> Tamaño </th>
+            <th> Fecha </th>
+            <th> Procesar </th>
+        </tr>
+    </thead>
+
+    <tbody id="destino_resultado">
+        <?php foreach ($files_imported_data as $data): ?>
+        <tr>
+            <td><?= $data['name']; ?></td>
+            <td><?= $data['size'].' bytes'; ?></td>
+            <td><?= date("d/m/Y H:i:s",$data['date']); ?></td>
+            <td><?= anchor('usuarios/movimientos/', ' ', array('class' => 'glyphicon glyphicon-play-circle')); ?></td>
+        </tr>
+        <?php endforeach; ?>
+    </tbody>
+</table>
